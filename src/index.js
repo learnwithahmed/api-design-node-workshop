88f586de95f75413b1278a8968dfce5d776d1e6e
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-08-15 13:16:50 +0200

    :wrench: [index] cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
-import config from './config'
 import cors from 'cors'
 
 export const app = express()
@@ -12,12 +11,7 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
-;(async () => {
-	try {
-		app.listen(config.port, () => {
-			console.log(`REST API on http://localhost:${config.port}/v1/api`)
-		})
-	} catch (e) {
-		console.error(e)
-	}
-})()
+
+const start = async () => {}
+
+start()
