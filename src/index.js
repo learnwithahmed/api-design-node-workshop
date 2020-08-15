8aab5d24827643433fef7000007ab6c017e9464b
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-08-15 16:54:29 +0200

    :wrench: [server] resolve conflict

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
+import config from './config'
 import cors from 'cors'
-
+import { connect } from './helpers/db'
 export const app = express()
 
 app.disable('x-powered-by')
@@ -12,6 +13,17 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-const start = async () => {}
+// Mount all routers here
+
+const start = async () => {
+	try {
+		await connect()
+		app.listen(config.port, () => {
+			console.log(`REST API on http://localhost:${config.port}/api`)
+		})
+	} catch (e) {
+		console.error(e)
+	}
+}
 
 start()
